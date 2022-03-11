2064f7218a529a22a91a57348bb07c535371e8e3
FengChi <hh541236@gmail.com>
2022-03-11 23:35:35 +0800

    config update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
 module.exports = {
   // 當目前的環境是 'production' 要使用 npm run build 時會跑 '/my-project/'路徑，如果不是跑'/'路徑
   publicPath: process.env.NODE_ENV === 'production'
